281abd451d93bb8be163e76af703cc522a38b9a9
ManuVleurickVUB <Manu.Gaston.R.Vleurick@vub.be>
2024-03-16 22:58:44 +0100

    Footer and NavBar added

---
+++ components/NavBar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const NavBar = () => {
+    return (
+        <nav className="flex-between max-container padding-container relative z-30 py-5">
+            <Link href="/">
+                <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+            </Link>
+
+            <ul className="hidden h-full gap-12 lg:flex">
+                {[]}
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
